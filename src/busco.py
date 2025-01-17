97db316892e579ab2164497e2021077e10c8d7f8
Sofía García Juan <sgarjua@gmail.com>
2025-01-17 12:56:47 +0100

    Update busco.py

--- src/busco.py
+++ src/busco.py
@@ -11,11 +11,11 @@ def run_busco(arguments):
         return {"command": command, "msg": "BUSCO already done",
                 "out_fpath": output_dir}
     else:
-        run_ = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+        run_ = subprocess.run(command, shell=True, stderr=subprocess.PIPE)
         if run_.returncode == 0:
             msg = "BUSCO run succesfully"
         else:
-            msg = "BUSCO Failed: \n {}".format(run_.stdout)
+            msg = "BUSCO Failed: \n {}".format(run_.stderr)
         return {"command": command, "msg": msg,
                 "out_fpath": output_dir}
 
