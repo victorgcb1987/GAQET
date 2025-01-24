d253ef0aea1447919d6c1f4b226225ce1ce96a40
Sofía García Juan <sgarjua@gmail.com>
2025-01-24 09:54:07 +0100

    first draft of agat script

---
+++ src/agat.py
@@ -0,0 +1,43 @@
+import subprocess
+
+def run_agat(arguments):
+    #Creating output dir
+    output_dir = arguments["output"] / "RunAgat" 
+    output_dir.mkdir()
+    out_fpath = output_dir / "ResultAgat.txt"
+
+    #Creating command to run AGAT as a list
+    cmd = ["agat_sp_statistics.pl", "-i", "{}".format(str(arguments["gff"])), "-o", "{}".format(str(out_fpath))]
+
+    #Adding "distribution" to command if it has been selected
+    if arguments["distribution"]:
+        dist_arg = ["-d"]
+        cmd += dist_arg
+    #Adding "plot" to command if it has been selected
+    if arguments["plot"]:
+        plot_arg = ["-p"]
+        cmd += plot_arg
+
+    #Adding "genome size" to command if it has a different value than default
+    if arguments["gsize"] > 0:
+        size_arg = ["-g {}".format(arguments["gsize"])]
+        cmd += size_arg
+
+#Check if AGAT is already done
+    if output_dir.exists():
+        #Show a message if it is
+        return {"command" cmd, "msg": "AGAT already done",
+                "out_fpath": out_fpath}
+    #But if is not done
+    else:
+        #Run AGAT with command
+        run_ = subprocess.run(command, shell=True, stderr=subprocess.PIPE)
+        #Is process has gone well
+        if run_.returncode == 0:
+            msg = "AGAT run succesfully"
+        #But if not
+        else:
+            msg = "AGAT Failed: \n {}".format(run_.stderr)
+        #Return command, final message and output dir path
+        return {"command": cmd, "msg": msg,
+                "out_fpath": out_fpath}
