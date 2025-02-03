873c82f86ab4a19eb0fccd5428338d84ff1c8a4d
Sofía García Juan <sgarjua@gmail.com>
2025-02-03 08:44:26 +0100

    Update LTR_inputs.py

--- src/LTR_inputs.py
+++ src/LTR_inputs.py
@@ -2,7 +2,13 @@ import subprocess
 
 def run_suffixerator(arguments):
     #output dir?
+    #suffixerator command
     command = "gt suffixerator -db {} -indexname {} -tis -suf -lcp -des -ssp -sds -dna".format(arguments["fasta"],
                                                                                                 arguments["gff"])
 
     print(command)
+
+    #run suffixerator
+    run_ = subprocess.run(command, shell=True, stderr=subprocess.PIPE)
+
+    return{"command":command, "retruncode":run_.returncode}
