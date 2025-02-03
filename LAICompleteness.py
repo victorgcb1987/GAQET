d199bd26c0345ad8011711e7d541215f9761904e
Sofía García Juan <sgarjua@gmail.com>
2025-02-03 10:42:19 +0100

    Update LAICompleteness.py

--- LAICompleteness.py
+++ LAICompleteness.py
@@ -4,6 +4,8 @@ import sys
 from pathlib import Path
 
 from src.LTR_inputs import run_suffixerator
+from src.LTR_inputs import run_harvest
+from src.LTR_inputs import run_finder
 #from src.LTR_retriever import
 
 def parse_arguments():
@@ -23,7 +25,9 @@ def parse_arguments():
 
     #help_infinder = ###
 
-    #help_output = ###
+    help_output = "(Required) Output path"
+    parser.add_argument("--out", "-o", type=str,
+                        help=help_output) #required=True
 
     if len(sys.argv)==1:
         parser.print_help()
@@ -35,7 +39,7 @@ def get_arguments():
     parser = parse_arguments()
     return {"fasta": Path(parser.fasta),
             "gff": Path(parser.gff),
-            } 
+            "output":Path(parser.out)} 
             ###
 
 
@@ -43,7 +47,9 @@ def main():
     arguments = get_arguments() 
     print(arguments)
     suffixerator = run_suffixerator(arguments)
-    print(suffixerator)
+    harvest = run_harvest(arguments)
+    finder = run_finder(arguments)
+
     #if resuls_harvester["returncode"] != 1 and resuls_finder
 
 
