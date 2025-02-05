3f0ab0557730f08e53e482be88f4a59ddd0cd805
Sofía García Juan <sgarjua@gmail.com>
2025-02-05 15:52:45 +0100

    Update LAICompleteness.py

--- LAICompleteness.py
+++ LAICompleteness.py
@@ -3,6 +3,7 @@ import sys
 
 from pathlib import Path
 
+from src.LTR_inputs import create_outdir
 from src.LTR_inputs import run_suffixerator
 from src.LTR_inputs import run_harvest
 from src.LTR_inputs import run_finder
